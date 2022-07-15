f0ec982f42d9a0693399a46afd2048c95ca6bf24
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2022-07-15 19:15:12 +0200

    updated style, removed all dummy data and updated dependencies

--- src/stores.ts
+++ src/stores.ts
@@ -5,42 +5,7 @@ import {writable} from 'svelte/store'
 export const MultiStore = writable(false);
 
 
-export const EntityStore = writable([
-        {
-        id: 3123123,
-        selected: false,
-        metadata: {
-            name: "Directed Graph 1",
-            type: "graph",
-            "type-long": "Graph",
-            "time-imported": "2022-06-17T10:10:17Z", 
-            "file-size": 23893,
-            "file-type": "cache"
-        },
-        instancedata: {
-            "rust-id": 3435234,
-            "nodes-count": 123,
-            "edges-count": 12300
-        },
-    },
-    {
-        id: 4123123,
-        selected: false,
-        metadata: {
-            name: "Data Table 1",
-            type: "table",
-            "type-long": "Data Table",
-            "time-imported": "2022-06-17T10:10:17Z", 
-            "file-size": 23893,
-            "file-type": "cache"
-        },
-        instancedata: {
-            "rust-id": 234234,
-            "rows": 123,
-            "columns": 123
-        },
-    }
-]);
+export const EntityStore = writable([]);
 
 
 export const TemplateEntityStore = writable({
