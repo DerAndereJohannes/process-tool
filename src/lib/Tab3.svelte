7fea4b6ff5102d4ddf4e11e5cc3fd1d158c3be85
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2022-07-19 14:12:27 +0200

    fix where selectall was crashing the application

---
+++ src/lib/Tab3.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    const JSONVIEW: string[] = ["ocel", "table"];
+    const GRAPHVIEW: string[] = ["ocdg", "graph"];
+
+    import {EntityStore} from '../stores';
+    import JsonViewer from "./JsonViewer.svelte";
+    import GraphViewer from "./GraphViewer.svelte";
+
+    $: entity = $EntityStore.find((item) => item.selected);
+
+</script>
+
+{#if entity}
+    {#if JSONVIEW.includes(entity.metadata.type)}
+        json my guy
+    {:else}
+        graph my guy
+    {/if}
+{/if}
